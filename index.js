ca2250c59f32c12dcb3c8005cf6da1693008a6ae
Aakash Rambhad <aakashrambhad08@gmail.com>
2024-05-24 19:16:42 +0530

    CRUD

--- index.js
+++ index.js
@@ -1,41 +1,71 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
+
+const data = JSON.parse(fs.readFileSync("data.json", "utf-8"));
+const products = data.products;
+
+app.use(express.json());
+
 
 app.use((req, res, next) => {
-    console.log(req.method, req.ip, req.get('User-Agent'), req.hostname)
+    // console.log(req.method, req.ip, req.get('User-Agent'), req.hostname)
     next()
 })
 
-const auth = (req, res, next) => {
-    // console.log(req.query);
 
-    if (req.query.password === '123') {
-        next()
-    } else {
-        res.sendStatus(401);
-    }
+const auth = (req, res, next) => {
+    next();
 }
 
 app.use(auth);
+//Products
+//API  ROOT , base URL
 
-app.get('/', (req, res) => {
-    res.json({ type: 'GET' })
+//Create POST / products   C R U D
+app.post('/products', (req, res) => {
+    console.log(req.body);
+    products.push(req.body);
+    res.status(201).json(req.body);
+
 })
 
-app.post('/', (req, res) => {
-    res.json({ type: 'POST' })
+
+// Read GET /products/:id
+app.get('/products/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const product = products.find(p => p.id === id);
+    res.json(product);
 })
 
-app.put('/', (req, res) => {
-    res.json({ type: 'PUT' })
+//Update PATCH / products   C R U D
+app.patch('/products/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const productIndex = products.findIndex(p => p.id === id)
+    const product = products[productIndex];
+    products.splice(productIndex, 1, { ...product, ...req.body })
+    res.status(201).json();
+});
+
+//Update PUT / products   C R U D
+app.put('/products/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const productIndex = products.find(p => p.id === id)
+    products.splice(productIndex, 1, { ...req.body, id: id })
+    res.status(201).json();
 })
 
-app.delete('/', (req, res) => {
-    res.json({ type: 'DELETE' })
+app.get('/products', (req, res) => {
+    res.json(products);
 })
 
-app.patch('/', (req, res) => {
-    res.json({ type: 'PATCH' })
+//delete DELETE /products/:id
+app.delete('/products/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const productIndex = products.findIndex(p => p.id === id)
+    const product = products[productIndex];
+    products.splice(productIndex, 1)
+    res.status(201).json(product);
 })
 
 app.listen(8080, () => {
